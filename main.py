2d09e1e59dcb607829ec6edeacae5494169c8c32
rjangid631 <137793021+rjangid631@users.noreply.github.com>
2023-12-17 19:11:52 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,31 @@
+import re
+import dns.resolver
+
+
+def is_valid_email(email):
+    # Define a regular expression for a valid email address
+    email_regex = r'^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$'
+
+    # Check if the email matches the regular expression
+    if not re.match(email_regex, email):
+        return False
+
+    # Split the email address to get the domain
+    _, domain = email.split('@')
+
+    # Check if the domain has valid DNS records
+    try:
+        dns.resolver.query(domain, 'MX')
+        return True
+    except dns.resolver.NXDOMAIN:
+        return False
+
+
+if __name__ == "__main__":
+    # Example usage
+    email_to_check = input("Enter an email address: ")
+
+    if is_valid_email(email_to_check):
+        print(f"{email_to_check} is a valid email address.")
+    else:
+        print(f"{email_to_check} is not a valid email address.")
